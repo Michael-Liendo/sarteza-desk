802b638cffececb37a713195148f6b2bac801cba
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-07-30 14:08:52 -0400

    feat: call to action

--- src/schemas/home.ts
+++ src/schemas/home.ts
@@ -1,21 +1,52 @@
 import { defineField } from "sanity";
-import {HomeIcon} from '@sanity/icons'
+import { HomeIcon } from "@sanity/icons";
 
-const TITLE = "Home"
+const TITLE = "Home";
 
 export default defineField({
-  name:'home',
-  title: TITLE,
-  type:'document',  
-  icon: HomeIcon,
-  groups: [{default:true, name:'editorial', title:'Editorial'}],
-  fields: [defineField({name:'hero', type:'string', group:'editorial'})],
-  preview: {
-    prepare() {
-      return {
-        // media: icon,
-        subtitle: 'Index',
-        title: TITLE,
-      }
-    },
-  }})
+	name: "home",
+	title: TITLE,
+	type: "document",
+	icon: HomeIcon,
+	groups: [{ default: true, name: "editorial", title: "Editorial" }],
+	fields: [
+		defineField({
+			name: "hero",
+			type: "array",
+			group: "editorial",
+			of: [
+				{
+					type: "object",
+					fields: [{ name: "src", type: "string", title: "Image" }],
+				},
+			],
+		}),
+		defineField({
+			name: "notification",
+			type: "array",
+			group: "editorial",
+			of: [{ type: "string" }],
+		}),
+		defineField({
+			type: "object",
+			name: "call_to_action",
+			title: "Call to Action",
+			group: "editorial",
+			fields: [
+				{ name: "src", type: "url", title: "Image" },
+				{ name: "title", type: "string", title: "Title" },
+				{ name: "small", type: "string", title: "Small" },
+				{ name: "button", type: "string", title: "Button" },
+			],
+		}),
+	],
+	preview: {
+		prepare() {
+			return {
+				media: HomeIcon,
+				subtitle: "Index",
+				title: TITLE,
+			};
+		},
+	},
+});
